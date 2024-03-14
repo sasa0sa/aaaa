1814e3c6680cb5041ef487f71da1870acbe0c87e
sasa0sa <151135533+sasa0sa@users.noreply.github.com>
2024-03-14 09:54:17 +0100

    a

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
-<html>
+<html style="font-family: 'Karla', sans-serif;">
+
     <head>
         <title>Laragon</title>
         <link href="/public/assets/bootstrap-5.0.2-dist/css/bootstrap.min.css" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Karla:400" rel="stylesheet" type="text/css">
         <link href="input.css" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@300..700&family=Jost:ital,wght@0,100..900;1,100..900&family=Karla:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet">
     </head>
 
     <body>
@@ -18,7 +22,8 @@
                 <img class="h-12 w-auto" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Escut_del_Pinell_de_Brai.svg/1200px-Escut_del_Pinell_de_Brai.svg.png" alt="">
             </a>
         </div>
-        <div class="hidden lg:flex lg:gap-x-16 flex flex-row items-center flex-grow">
+        
+        <div class="hidden lg:flex lg:gap-x-16 pl-10 flex flex-row items-center flex-grow">
             <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Informació</a>
             <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Llocs d'interés</a>
             <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Altra informació</a>
@@ -26,12 +31,12 @@
             <!-- component -->
             <div class="relative hover-trigger text-sm font-semibold leading-6 text-gray-900">
             <svg xmlns="http://www.w3.org/2000/svg" class="mt-0.5 animate-pulse" width="24" height="24"><path d="M12 17.414 3.293 8.707l1.414-1.414L12 14.586l7.293-7.293 1.414 1.414L12 17.414z"/></svg>
-              <div class="absolute rounded-md -bottom-20 right-16 bg-white border border-grey-100 px-4 py-2 hover-target text-sm font-semibold leading-6 text-gray-900">
-                <div class="hidden lg:flex lg:gap-x-24 flex flex-row items-center flex-grow">
-                  <a href="#" class="text-sm font-semibold leading-6 text-gray-900 text-center">Informació</a>
-                  <a href="#" class="text-sm font-semibold leading-6 text-gray-900 text-center">Llocs d'interés</a>
-                  <a href="#" class="text-sm font-semibold leading-6 text-gray-900 text-center">Altra informació</a>
-                  <a href="#" class="text-sm font-semibold leading-6 text-gray-900 text-center">Alcaldes</a>
+              <div class="absolute rounded-md -bottom-20 right-0.5 bg-white border-b border-grey-100 px-4 py-2 hover-target text-sm font-semibold leading-6 text-gray-900">
+                <div class="hidden lg:flex lg:gap-x-32 flex flex-row items-center flex-grow justify-center">
+                  <a href="#" class="text-sm font-semibold leading-6 text-gray-400 text-center">Informació</a>
+                  <a href="#" class="text-sm font-semibold leading-6 text-gray-400 text-center">Llocs d'interés</a>
+                  <a href="#" class="text-sm font-semibold leading-6 text-gray-400 text-center">Altra informació</a>
+                  <a href="#" class="text-sm font-semibold leading-6 text-gray-400 text-center">Alcaldes</a>
                 </div>  
               </div>
             </div>
@@ -62,9 +67,21 @@
     <div class="bg-white py-24 sm:py-32">
   <div class="mx-auto max-w-7xl px-6 lg:px-8 mt-10">
     <div class="mx-auto max-w-2xl sm:text-center">
-      <h2 class="text-3xl font-bold text-gray-900 sm:text-4xl tracking-wide">AJUNTAMENT DEL PINELL DE BRAI</h2>
+      <h2 class="text-3xl font-bold text-gray-900 sm:text-4xl tracking-wide" >AJUNTAMENT DEL PINELL DE BRAI</h2>
       <p class="mt-8 text-lg leading-8 text-gray-600">Som l'ajuntament del Pinell de brai. Un poble de la Terra Alta.</p>
     </div>
+    <form class="flex items-center max-w-sm mx-auto mt-6">   
+      <label for="simple-search" class="sr-only">Search</label>
+      <div class="relative w-full">
+          <input type="text" id="simple-search" class="outline-none bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Busca el que vulguis..." required />
+      </div>
+      <button type="submit" class="p-2.5 ms-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+          </svg>
+          <span class="sr-only">Search</span>
+      </button>
+    </form>
     <div class="mx-auto mt-16 max-w-2xl rounded-3xl ring-1 ring-gray-200 sm:mt-20 lg:mx-0 lg:flex lg:max-w-none">
       <div class="p-8 sm:p-10 lg:flex-auto">
         <h3 class="text-2xl font-bold tracking-tight text-gray-900">Lifetime membership</h3>
@@ -117,6 +134,120 @@
   </div>
 </div>
 </div> 
+<!-- component -->
+<div class="border-b border-grey-100"></div>
+<footer class="text-gray-600 body-font">
+  <div class="container px-5 py-20 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
+    <div class="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left md:mt-0 mt-10">
+      <a class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+          <img class="h-12 w-auto text-white mb-4" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Escut_del_Pinell_de_Brai.svg/1200px-Escut_del_Pinell_de_Brai.svg.png" alt="">
+        <span class="ml-3 text-xl mb-2">Footer</span>
+      </a>
+      <p class="mt-2 text-sm text-gray-500 mb-2">Air plant banjo lyft occupy retro adaptogen indego</p>
+    </div>
+    <div class="flex-grow flex flex-wrap md:pr-20 -mb-10 md:text-left text-center order-first">
+      <div class="lg:w-1/4 md:w-1/2 w-full px-4">
+        <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
+        <nav class="list-none mb-10">
+          <li>
+            <a class="text-gray-600 hover:text-gray-800">First Link</a>
+          </li>
+          <li>
+            <a class="text-gray-600 hover:text-gray-800">Second Link</a>
+          </li>
+          <li>
+            <a class="text-gray-600 hover:text-gray-800">Third Link</a>
+          </li>
+          <li>
+            <a class="text-gray-600 hover:text-gray-800">Fourth Link</a>
+          </li>
+        </nav>
+      </div>
+      <div class="lg:w-1/4 md:w-1/2 w-full px-4">
+        <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
+        <nav class="list-none mb-10">
+          <li>
+            <a class="text-gray-600 hover:text-gray-800">First Link</a>
+          </li>
+          <li>
+            <a class="text-gray-600 hover:text-gray-800">Second Link</a>
+          </li>
+          <li>
+            <a class="text-gray-600 hover:text-gray-800">Third Link</a>
+          </li>
+          <li>
+            <a class="text-gray-600 hover:text-gray-800">Fourth Link</a>
+          </li>
+        </nav>
+      </div>
+      <div class="lg:w-1/4 md:w-1/2 w-full px-4">
+        <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
+        <nav class="list-none mb-10">
+          <li>
+            <a class="text-gray-600 hover:text-gray-800">First Link</a>
+          </li>
+          <li>
+            <a class="text-gray-600 hover:text-gray-800">Second Link</a>
+          </li>
+          <li>
+            <a class="text-gray-600 hover:text-gray-800">Third Link</a>
+          </li>
+          <li>
+            <a class="text-gray-600 hover:text-gray-800">Fourth Link</a>
+          </li>
+        </nav>
+      </div>
+      <div class="lg:w-1/4 md:w-1/2 w-full px-4">
+        <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
+        <nav class="list-none mb-10">
+          <li>
+            <a class="text-gray-600 hover:text-gray-800">First Link</a>
+          </li>
+          <li>
+            <a class="text-gray-600 hover:text-gray-800">Second Link</a>
+          </li>
+          <li>
+            <a class="text-gray-600 hover:text-gray-800">Third Link</a>
+          </li>
+          <li>
+            <a class="text-gray-600 hover:text-gray-800">Fourth Link</a>
+          </li>
+        </nav>
+      </div>
+    </div>
+  </div>
+  <div class="bg-gray-100">
+    <div class="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
+      <p class="text-gray-500 text-sm text-center sm:text-left">© 2020 Footer —
+        <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" class="text-gray-600 ml-1" target="_blank">@MrsRobbot</a>
+      </p>
+      <span class="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
+        <a class="text-gray-500">
+          <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+            <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+          </svg>
+        </a>
+        <a class="ml-3 text-gray-500">
+          <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+            <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+          </svg>
+        </a>
+        <a class="ml-3 text-gray-500">
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+            <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+            <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+          </svg>
+        </a>
+        <a class="ml-3 text-gray-500">
+          <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="0" class="w-5 h-5" viewBox="0 0 24 24">
+            <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
+            <circle cx="4" cy="4" r="2" stroke="none"></circle>
+          </svg>
+        </a>
+      </span>
+    </div>
+  </div>
+</footer>
 
 </body>
 </html>
